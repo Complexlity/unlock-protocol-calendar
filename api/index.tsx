a3f555b542b8c1b9e8d9aa6e9c908cf1b1b5268f
Complexlity <nwalozielijah@gmail.com>
2024-12-04 16:42:28 +0100

    feat: calendar and mint pages

--- api/index.tsx
+++ api/index.tsx
@@ -3,7 +3,11 @@ import { devtools } from "frog/dev";
 // import { neynar } from 'frog/hubs'
 import { handle } from "frog/next";
 import { serveStatic } from "frog/serve-static";
-import { getCurrentDateUTC, getUserNftBalance } from "./utils.js";
+import {
+  getCurrentDateUTC,
+  getUserNftBalance,
+  getValidKeysForUser,
+} from "./utils.js";
 import { DAYS_CONTRACT_ADDRESSES } from "./constants.js";
 import uniFarcasterSdk from "../node_modules/uni-farcaster-sdk/dist/index.mjs";
 import { Address } from "viem";
@@ -61,14 +65,43 @@ app.frame("/", (c) => {
       </div>
     ),
     intents: [
-      <Button action="/mint">Mint Today</Button>,
-      <Button action="/calendar">View Calendar</Button>,
+      // <Button action="/mint">Mint Today</Button>,
+      <Button action="/calendar">Start</Button>,
     ],
   });
 });
 
-app.frame("/calendar", (c) => {
+app.frame("/calendar", async (c) => {
+  const frameData = c.frameData;
+  if (!frameData) {
+    return c.error({
+      message: "Frame data missing",
+    });
+  }
+  const userFid = frameData.fid;
+  // const res = await sdkInstance.getUsersByFid([userFid]);
+  // if (res.error) {
+  //   return c.error({
+  //     message: "Something went wrong getting your user data",
+  //   });
+  // }
+  // const userAddress = res.data[0].ethAddresses[0];
+  // const userAddress = "0x8ff47879d9eE072b593604b8b3009577Ff7d6809" as Address;
+  const userAddress = "0xe06Dacf8a98CBbd9692A17fcb8e917a6cb5e65ED" as Address;
+  const today = getCurrentDateUTC();
+  const validKeys = await getValidKeysForUser(userAddress, today);
+  console.log({ validKeys });
+  let nextMintableDay = -1;
+  for (let i = 0; i < today; i++) {
+    const hasMembership = validKeys[i];
+    const hasPreviousMembership = i === 0 ? true : validKeys[i - 1];
+    if (!hasMembership && hasPreviousMembership) {
+      nextMintableDay = i;
+    }
+  }
+
   return c.res({
+    action: `/finish/${nextMintableDay}`,
     image: (
       <div
         style={{
@@ -100,62 +133,104 @@ app.frame("/calendar", (c) => {
         </div>
       </div>
     ),
-    intents: [<Button.Transaction target="/tx">Start</Button.Transaction>],
-  });
-});
-
-app.frame("/mint", async (c) => {
-  const frameData = c.frameData;
-  if (!frameData) {
-    return c.error({ message: "Frame data missing from request" });
-  }
-  const fid = frameData.fid;
-  // const res = await sdkInstance.getUsersByFid([fid]);
-  // if (res.error) {
-  //   console.log(res.error);
-  //   return c.error({ message: "Could` not get user address" });
-  // }
-  // const userAddress = res.data[0].ethAddresses[0] as Address;
-  const userAddress = "0x8ff47879d9eE072b593604b8b3009577Ff7d6809" as Address;
-  const currentDay = getCurrentDateUTC();
-  console.log({ currentDay });
-  const currentDayContract = DAYS_CONTRACT_ADDRESSES[currentDay - 1];
-  console.log({ currentDayContract });
-  const userBalance = await getUserNftBalance(
-    userAddress,
-    currentDayContract
-  ).catch((e) => null);
-  if (userBalance === null) {
-    return c.error({
-      message: "Something went wrong getting balance",
-    });
-  }
-
-  if (Number(userBalance) == 1) {
-    return c.error({
-      message: "You already minted today's NFT",
-    });
-  }
-
-  return c.res({
-    image: "https://i.ibb.co/xfnR1cL/frame.png",
     intents: [
-      <Button.Transaction target={`/tx/${currentDay}/${userAddress}`}>
-        Mint Today
+      <Button.Transaction target={`/tx/${nextMintableDay}/${userAddress}`}>
+        Mint Day {`${Number(nextMintableDay) + 1}`}
       </Button.Transaction>,
     ],
   });
 });
 
+app.frame("/finish/:day", async (c) => {
+  const day = c.req.param("day");
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background: "linear-gradient(to right, #432889, #17101F)",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+            display: "flex",
+          }}
+        >
+          You successfully minted day {`${Number(day) + 1}`}
+        </div>
+      </div>
+    ),
+    intents: [<Button action="/calendar">View Calendar</Button>],
+  });
+});
+// app.frame("/mint", async (c) => {
+//   const frameData = c.frameData;
+//   if (!frameData) {
+//     return c.error({ message: "Frame data missing from request" });
+//   }
+//   const fid = frameData.fid;
+//   // const res = await sdkInstance.getUsersByFid([fid]);
+//   // if (res.error) {
+//   //   console.log(res.error);
+//   //   return c.error({ message: "Could` not get user address" });
+//   // }
+//   // const userAddress = res.data[0].ethAddresses[0] as Address;
+//   const userAddress = "0x8ff47879d9eE072b593604b8b3009577Ff7d6809" as Address;
+//   const currentDay = getCurrentDateUTC();
+//   console.log({ currentDay });
+//   const currentDayContract = DAYS_CONTRACT_ADDRESSES[currentDay - 1];
+//   console.log({ currentDayContract });
+//   const userBalance = await getUserNftBalance(
+//     userAddress,
+//     currentDayContract
+//   ).catch((e) => null);
+//   if (userBalance === null) {
+//     return c.error({
+//       message: "Something went wrong getting balance",
+//     });
+//   }
+
+//   if (Number(userBalance) == 1) {
+//     return c.error({
+//       message: "You already minted today's NFT",
+//     });
+//   }
+
+//   return c.res({
+//     image: "https://i.ibb.co/xfnR1cL/frame.png",
+//     intents: [
+//       <Button.Transaction target={`/tx/${currentDay}/${userAddress}`}>
+//         Mint Today
+//       </Button.Transaction>,
+//     ],
+//   });
+// });
+
 app.transaction("/tx/:day/:address", async (c) => {
   const { day, address } = c.req.param();
   const userAddress = address as Address;
-  const currentDay = Number(day);
-  if (isNaN(currentDay)) {
+  const nextMintableDay = Number(day);
+  if (isNaN(nextMintableDay)) {
     return c.error({ message: "Invalid day" });
   }
-  console.log({ currentDay });
-  const currentDayContract = DAYS_CONTRACT_ADDRESSES[currentDay - 1];
+  console.log({ currentDay: nextMintableDay });
+  const currentDayContract = DAYS_CONTRACT_ADDRESSES[nextMintableDay];
   console.log({ currentDayContract });
   const userBalance = await getUserNftBalance(
     userAddress,
